d3ff08371c32f8a5dd789d60827522a87687ea0e
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 14:08:27 +0700

    Encoder.ts

---
+++ src/binary/Encoder.ts
@@ -0,0 +1,15 @@
+/**
+ * Binary Encoder
+ */
+
+import type { BinaryNode } from "./BinaryNode"
+
+export function encodeBinaryNode(
+    node: BinaryNode
+): Uint8Array {
+
+    throw new Error(
+        "encodeBinaryNode() not implemented."
+    )
+
+}
