6f0cd86ad7aafb6e6bb44c7fe668bf319224981d
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 11:05:45 +0700

    credentials.ts

--- src/auth/credentials.ts
+++ src/auth/credentials.ts
@@ -1,8 +1,32 @@
-/**
- * Credentials
- *
- * TODO:
- * Implement credentials manager.
- */
+export interface AuthenticationCreds {
+  noiseKey: Uint8Array
+  identityKey: Uint8Array
+  signedIdentityKey: Uint8Array
 
-export {}
+  registrationId: number
+
+  advSecretKey: string
+
+  me?: {
+    id: string
+    name?: string
+  }
+
+  account?: unknown
+
+  signalIdentities: unknown[]
+
+  platform: string
+
+  processedHistoryMessages: unknown[]
+
+  nextPreKeyId: number
+
+  firstUnuploadedPreKeyId: number
+
+  accountSyncCounter: number
+
+  lastAccountSyncTimestamp: number
+
+  myAppStateKeyId: string
+}
