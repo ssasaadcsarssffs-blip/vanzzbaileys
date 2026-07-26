37963d356c0947a808d4b98e597bad8eea429d1c
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 14:09:08 +0700

    Tags.ts

---
+++ src/binary/Tags.ts
@@ -0,0 +1,19 @@
+/**
+ * Binary Tags
+ */
+
+export enum BinaryTag {
+
+    LIST_EMPTY = 0x00,
+
+    STREAM_END = 0x02,
+
+    DICTIONARY_0 = 0xec,
+
+    DICTIONARY_1 = 0xed,
+
+    DICTIONARY_2 = 0xee,
+
+    DICTIONARY_3 = 0xef
+
+}
