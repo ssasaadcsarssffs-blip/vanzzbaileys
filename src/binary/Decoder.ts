c735056319ad58593993b6c3ba0dd53963373c41
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 14:07:44 +0700

    Decoder.ts

---
+++ src/binary/Decoder.ts
@@ -0,0 +1,15 @@
+/**
+ * Binary Decoder
+ */
+
+import type { BinaryNode } from "./BinaryNode"
+
+export function decodeBinaryNode(
+    data: Uint8Array
+): BinaryNode {
+
+    throw new Error(
+        "decodeBinaryNode() not implemented."
+    )
+
+}
