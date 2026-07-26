9ecd0e580d63bf68ca0cd58ab19ab333a654d3f6
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 13:51:09 +0700

    assert.ts

---
+++ src/utils/assert.ts
@@ -0,0 +1,12 @@
+/**
+ * Assert utility.
+ */
+
+export function assert(
+    condition: unknown,
+    message = "Assertion failed"
+): asserts condition {
+    if (!condition) {
+        throw new Error(message)
+    }
+}
