1e556e1618da3142f4c3598f8167ef989d857626
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 11:06:13 +0700

    keys.ts

--- src/auth/keys.ts
+++ src/auth/keys.ts
@@ -1,8 +1,20 @@
-/**
- * Signal Keys
- *
- * TODO:
- * Implement key storage.
- */
+export type SignalKeyType =
+  | "pre-key"
+  | "session"
+  | "sender-key"
+  | "app-state-sync-key"
+  | "app-state-sync-version"
+  | "sender-key-memory"
 
-export {}
+export interface SignalKeyStore {
+  get<T>(
+    type: SignalKeyType,
+    ids: string[]
+  ): Promise<Record<string, T>>
+
+  set(
+    data: Record<SignalKeyType, Record<string, unknown>>
+  ): Promise<void>
+
+  clear?(): Promise<void>
+}
