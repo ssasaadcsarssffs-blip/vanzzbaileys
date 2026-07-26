ad8b4cede560ca21903200cfabf1f56522aeb354
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 11:07:52 +0700

    state.ts

--- src/auth/state.ts
+++ src/auth/state.ts
@@ -1,8 +1,7 @@
-/**
- * Authentication State
- *
- * TODO:
- * Implement authentication state manager.
- */
+import type { AuthenticationState } from "./storage"
 
-export {}
+export async function initAuthState(): Promise<AuthenticationState> {
+  throw new Error(
+    "initAuthState() has not been implemented yet."
+  )
+}
