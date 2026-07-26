1da2314f539f68ae607afac7ebead0e98cd2f367
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 13:40:18 +0700

    browser.ts

---
+++ src/defaults/browser.ts
@@ -0,0 +1,13 @@
+/**
+ * Default browser information.
+ */
+
+export const Browsers = {
+  ubuntu: ["Ubuntu", "Chrome", "22.0.0"] as [string, string, string],
+
+  windows: ["Windows", "Chrome", "22.0.0"] as [string, string, string],
+
+  macOS: ["macOS", "Chrome", "22.0.0"] as [string, string, string],
+
+  vanzz: ["VANZZ-BOT", "Chrome", "1.0.0"] as [string, string, string]
+}
