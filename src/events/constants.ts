6789bc0d876e2062e4928da78897bd44a45f8fb1
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 14:02:15 +0700

    constants.ts

---
+++ src/events/constants.ts
@@ -0,0 +1,15 @@
+/**
+ * Event Names
+ */
+
+export const CONNECTION_UPDATE = "connection.update"
+
+export const CREDS_UPDATE = "creds.update"
+
+export const MESSAGE_UPSERT = "messages.upsert"
+
+export const MESSAGE_UPDATE = "messages.update"
+
+export const MESSAGE_DELETE = "messages.delete"
+
+export const MESSAGE_REACTION = "messages.reaction"
