f277e931c5fd3265e6fbf1562bb32917cce8cc74
ssasaadcsarssffs-blip <ssasaadcsarssffs@gmail.com>
2026-07-26 08:00:52 +0700

    index.ts

---
+++ examples/pairing-code/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Pairing Code Example
+ *
+ * TODO:
+ * Implement after authentication module is ready.
+ */
+
+// import { VanzzBaileys } from "../../src"
+
+// const client = new VanzzBaileys({
+//   auth: "./auth"
+// })
+
+// await client.connect()
+
+// await client.requestPairingCode("6281234567890")
+
+export {}
